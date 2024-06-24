15225f8cc160566acf4b1f1d63f33f090f2df869
Benjamin Li <nicky01.bl@gmail.com>
2024-06-24 16:31:32 +0200

    arrange folders

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
-import { getUsers } from "@/app/api/users";
+import { getUsers } from "@/api/users";
 import { Button } from "@mantine/core";
 
 export default async function Home() {
   const users = await getUsers()
+  console.log("first")
   console.log(users)
   return <Button>hello world</Button>;
 }
