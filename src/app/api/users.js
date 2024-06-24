bd1006022e7422c60de090b8f93500a422f04a84
Benjamin Li <nicky01.bl@gmail.com>
2024-06-24 16:07:24 +0200

    added prisma & mantine

---
+++ src/app/api/users.js
@@ -0,0 +1,7 @@
+import { cache } from "react";
+import prisma from "../../../lib/prisma";
+
+export const getUsers = cache(async () => {
+  const users = await prisma.user.findMany()
+  return users;
+});
