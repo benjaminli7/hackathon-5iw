ca5073f618cd7e82a406bd6751fe082590ddfab8
syalicheff <yalicheff.sebastien@gmail.com>
2024-06-26 14:31:07 +0200

    test change réponseé

--- src/app/utils.js
+++ src/app/utils.js
@@ -2,6 +2,10 @@ import prisma from "../../lib/prisma";
 import { cache } from "react";
 // import useOpenAI from "@/hooks/useOpenAI";
 import { OpenAI } from "openai";
+const json = require("../../prisma/conversation.json");
+const jeune = json[0];
+const vieux = json[1];
+// console.log(adulte);
 
 const openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
@@ -20,22 +24,57 @@ export const getUsers = cache(async () => {
 //   });
 // });
 
-export const getAgeFromPrompt = cache(async (discussion, name) => {
-  return await openai.chat.completions.create({
+
+
+export const setEstimatedAgeFromPrompt = cache(async (discussion, name) => {
+  const response = await openai.chat.completions.create({
     messages: [
       {
         role: "user",
-        content:
-          "Essais de déterminer la tranche d'âge de la personne en fonction de cette discussions" +
-          discussion +
-          " prend en compte que la personne s'appelle " +
-          name +
-          " dans tout les cas tu doit trancher et ne jamais dire que tu ne sais pas ou que tu ne peux pas le déterminer ne renvoie qu'un age et rien d'autre",
+        // response_format: { type: "json_object" },
+        content: `Estime l'âge de la personne en fonction de cette discussions ${JSON.stringify(
+          vieux
+        )} ${
+          name ? "dans cette conversation le client s'appelle " + name : ""
+        } Répond obligatoirement par l'age de la personne ne dit pas que tu ne sais pas si tu ne sait pas éstime l'age de la personne, tu dois ne donner que l'age de la personne dans la réponse par exemple: 45`,
       },
     ],
-    model: "gpt-3.5-turbo",
+    model: "gpt-4o",
   });
+  const age = response.choices[0]?.message?.content;
+  const ageMatch = age.match(/\b\d+\b ans/i);
+  const estimatedAge = ageMatch ? parseInt(ageMatch[0]) : parseInt(age);
+  
+
+  return estimatedAge;
 });
+
+export const changeBotAnswerFromUserInfo = cache(async (answer, name) => {
+
+  const client = await prisma.user.findUnique({
+    where: {
+      name: name,
+    },
+  });
+
+  const response = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+
+        content: `
+          Voici la liste des informations du client ${name}:
+          ${JSON.stringify(client)}
+          La réponse prédéfinie qui devait être données par le bot était: ${answer}
+          CHANGE la formulation de la réponse prédéfinie en fonction des informations du client ci-dessus.
+          par exemple si la personne est plus agée que 50 ans tu utilise un vocabulaire plus formel et donne plus de détails sur la réponse.
+          Si la personne est plus jeune que 50 ans tu utilise un vocabulaire plus familier et va droit au but.
+        `,
+      },
+    ],
+    model: "gpt-4o",
+  });
+
 
 // getAgeFromPrompt("Salut, comment ça va ?", "Robert").then((res) => {
 //   console.log(res);
