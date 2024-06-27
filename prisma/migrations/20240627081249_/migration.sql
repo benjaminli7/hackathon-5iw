74adbdd270eebcc6c754b7618af47b3702336024
syalicheff <yalicheff.sebastien@gmail.com>
2024-06-27 16:04:37 +0200

    Fixes

--- prisma/migrations/20240627081249_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "age" INTEGER,
-ADD COLUMN     "isEstimated" INTEGER NOT NULL DEFAULT 0;
--- prisma/migrations/20240627083048_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `isEstimated` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "isEstimated",
-ADD COLUMN     "isEstimated" BOOLEAN NOT NULL DEFAULT false;
