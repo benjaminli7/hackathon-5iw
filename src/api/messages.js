a086819235cb4a8a729069b31be03ecdf3cce015
syalicheff <yalicheff.sebastien@gmail.com>
2024-06-28 15:01:33 +0200

    Assemblage

--- src/api/messages.js
+++ src/api/messages.js
@@ -1,6 +1,8 @@
 "use server";
 import prisma from "../../lib/prisma";
 import { cache } from "react";
+import { changeBotAnswerFromUserInfo } from "@/app/utils";
+import { cp } from "fs";
 
 export const getMsg = cache(async (userId) => {
   const user = await prisma.user.findUnique({
@@ -10,15 +12,101 @@ export const getMsg = cache(async (userId) => {
   return user;
 });
 
-export const postMsg = cache(async (userId, content, sender) => {
+export const postMsg = cache(async (userId, content, sender, operation) => {
   const message = await prisma.message.create({
     data: {
       userId: parseInt(userId, 10),
       sender: sender,
+      operation: operation,
       content: content,
-      contentIA: '',
+      contentIA: "",
     },
   });
 
-  return message;
+  // get the script by operation
+  const script = await prisma.conversationScript.findMany({
+    where: {
+      operation: operation,
+    },
+  });
+  console.log("Le script", script);
+  //check if the ConversationScript exist for the script/ user
+  const userScript = await prisma.userScriptProgress.findFirst({
+    where: {
+      userId: parseInt(userId, 10),
+      scriptId: script[0].id,
+    },
+  });
+  console.log("Le userScript", userScript);
+  let updatedUserScript = null;
+  if (userScript) {
+    console.log("on met a jour le script");
+    //currentStep = currentStep + 1
+    updatedUserScript = await prisma.userScriptProgress.update({
+      where: {
+        id: userScript.id,
+      },
+      data: {
+        currentStep: userScript.currentStep + 1,
+      },
+    });
+  } else {
+    //create a new userScriptProgress
+    console.log("on crée un nouveau script");
+    updatedUserScript = await prisma.userScriptProgress.create({
+      data: {
+        userId: parseInt(userId, 10),
+        scriptId: script[0].id,
+        currentStep: 1,
+      },
+    });
+  }
+
+  console.log("On récupère le script de l'operation");
+  // get the next step of the script where order = currentStep
+  const nextStep = await prisma.conversationScript.findFirst({
+    where: {
+      operation: operation,
+      order: updatedUserScript.currentStep,
+    },
+  });
+
+  // push the next to the message
+  if (!nextStep) {
+    console.log("Pas de prochaine étape on envoie le questionnaire");
+
+    //create the message with the link to the form and return
+    console.log("On ajoute le lien du questionnaire a la conversation");
+    const msgLink = `Merci de répondre a ce questionnaire <a href="http://localhost:3000/questionnaires/1/answer?id=${userId}">questionnaire</a>`;
+    const botmsg = await prisma.message.create({
+      data: {
+        userId: parseInt(userId, 10),
+        sender: "bot",
+        operation: operation,
+        content: msgLink,
+        contentIA: "",
+      },
+    });
+    return { message, botmsg };
+  }
+
+  // on adapte la réponse du bot en fonction des informations du client
+  const newFormulation = await changeBotAnswerFromUserInfo(
+    nextStep.content,
+    userId
+  );
+
+  //On créé le message
+  console.log("On créé le message du bot");
+  const botmsg = await prisma.message.create({
+    data: {
+      userId: parseInt(userId, 10),
+      sender: "bot",
+      operation: operation,
+      content: nextStep.content,
+      contentIA: newFormulation,
+    },
+  });
+
+  return { message, botmsg };
 });
--- src/app/chat/components/ChatBox.js
+++ src/app/chat/components/ChatBox.js
@@ -1,68 +1,112 @@
 "use client";
 
-import { useState, useEffect, useRef } from 'react';
-import { ScrollArea, TextInput, Button, Box, Stack, Text } from '@mantine/core';
-import { postMsg } from '@/api/messages';
+import { useState, useEffect, useRef } from "react";
+import { ScrollArea, TextInput, Button, Box, Stack, Text } from "@mantine/core";
+import { postMsg } from "@/api/messages";
+import { setEstimatedAgeFromPrompt } from "@/app/utils";
+import { patchAge } from "@/api/actions";
 
 const ChatBox = ({ userId, initialMessages = [], userName }) => {
-    const [messages, setMessages] = useState(initialMessages);
-    const [input, setInput] = useState('');
-    const scrollAreaRef = useRef(null);
+  //   console.log("ChatBox -> initialMessages", initialMessages);
+  const [messages, setMessages] = useState(initialMessages);
+  const [estimatedAge, setEstimatedAge] = useState(null);
+  const [input, setInput] = useState("");
+  const scrollAreaRef = useRef(null);
+  //estimation de l'age
+  useEffect(() => {
+    async function setAgeUser() {
+      const estimatedAge = await setEstimatedAgeFromPrompt(messages, userId);
+      // alert(estimatedAge);
+      if (estimatedAge) await patchAge(userId, estimatedAge);
+      // console.log("estimatedAge", estimatedAge);
+      setEstimatedAge(estimatedAge);
+    }
+    setAgeUser();
+  }, []);
 
-    useEffect(() => {
-        if (scrollAreaRef.current) {
-            scrollAreaRef.current.scrollTo({
-                top: scrollAreaRef.current.scrollHeight,
-                behavior: 'smooth'
-            });
-        }
-    }, [messages]);
+  useEffect(() => {
+    if (scrollAreaRef.current) {
+      scrollAreaRef.current.scrollTo({
+        top: scrollAreaRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
 
-    const sendMessage = async () => {
-        if (input.trim() === '') return;
+  const sendMessage = async () => {
+    if (input.trim() === "") return;
 
-        const newMessage = {
-            content: input,
-            sender: userName,
-        };
-
-        setMessages([...messages, newMessage]);
-
-        try {
-            await postMsg(userId, input, userName);
-            setInput('');
-        } catch (error) {
-            console.error('Failed to send message:', error);
-        }
+    const newMessage = {
+      content: input,
+      sender: userName,
     };
 
-    return (
-        <Box style={{ display: 'flex', flexDirection: 'column', height: '70vh' }}>
-            <ScrollArea style={{ flexGrow: 1, marginBottom: '1rem', padding: '1rem', border: '1px solid #ccc', borderRadius: '8px', backgroundColor: '#f9f9f9' }} viewportRef={scrollAreaRef}>
-                <Stack spacing="sm">
-                    {messages.map((msg, index) => (
-                        <Box key={index} mb="sm">
-                            <Text weight={700} color={msg.sender === userName ? 'blue' : 'gray'}>
-                                {msg.sender}:
-                            </Text>
-                            <Text>{msg.content}</Text>
-                        </Box>
-                    ))}
-                </Stack>
-            </ScrollArea>
-            <Box style={{ display: 'flex', alignItems: 'center' }}>
-                <TextInput
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                    placeholder="Type your message..."
-                    style={{ flexGrow: 1, marginRight: '0.5rem' }}
-                />
-                <Button onClick={sendMessage} color="blue">
-                    Send
-                </Button>
+    try {
+      const sendedMessages = await postMsg(
+        parseInt(userId),
+        input,
+        userName,
+        messages[0].operation
+      );
+
+      setMessages((prev) => [...prev, sendedMessages.message]);
+
+      if (sendedMessages.botmsg) {
+        setMessages((prev) => [...prev, sendedMessages.botmsg]);
+      }
+
+      setInput("");
+    } catch (error) {
+      console.error("Failed to send message:", error);
+    }
+  };
+
+  return (
+    <Box style={{ display: "flex", flexDirection: "column", height: "70vh" }}>
+      {estimatedAge && (
+        <Text align="center" weight={700} mb="sm" c="blue">
+          D'après notre IA, l'age estimé de cette personne est de {estimatedAge}
+        </Text>
+      )}
+      <ScrollArea
+        style={{
+          flexGrow: 1,
+          marginBottom: "1rem",
+          padding: "1rem",
+          border: "1px solid #ccc",
+          borderRadius: "8px",
+          backgroundColor: "#f9f9f9",
+        }}
+        viewportRef={scrollAreaRef}
+      >
+        <Stack spacing="sm">
+          {messages.map((msg, index) => (
+            <Box key={index} mb="sm">
+              <Text weight={700} c={msg.sender != "bot" ? "blue" : "gray"}>
+                {msg.sender != "bot" ? userName : "Bot"}
+              </Text>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: msg.contentIA ? msg.contentIA : msg.content,
+                }}
+              />{" "}
             </Box>
-        </Box>
-    );
+          ))}
+        </Stack>
+      </ScrollArea>
+      <Box style={{ display: "flex", alignItems: "center" }}>
+        <TextInput
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Type your message..."
+          style={{ flexGrow: 1, marginRight: "0.5rem" }}
+        />
+        <Button onClick={sendMessage} color="blue">
+          Send
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default ChatBox;
